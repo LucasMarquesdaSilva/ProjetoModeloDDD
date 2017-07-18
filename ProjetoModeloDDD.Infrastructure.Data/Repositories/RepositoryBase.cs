78f57594535935387309280f58833ae56b4bed0e
lmarques <lmarques@WKSJUN000467>
2017-07-18 19:07:01 -0300

    AutoMapper

---
+++ ProjetoModeloDDD.Infrastructure.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,52 @@
+﻿using ProjetoModeloDDD.Domain.Interfaces;
+using ProjetoModeloDDD.Infrastructure.Data.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.Infrastructure.Data.Repositories
+{
+    public class RepositoryBase<TEntity> : IDisposable, IRepositoryBase<TEntity> where TEntity : class
+    {
+        protected ProjetoModelContext Db = new ProjetoModelContext();
+        public void Add(TEntity obj)
+        {
+            Db.Set<TEntity>().Add(obj);
+            Db.SaveChanges();
+        }
+
+        public void Disposable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Db.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return Db.Set<TEntity>().Find(id);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            Db.Set<TEntity>().Remove(obj);
+            Db.SaveChanges();
+        }
+
+        public void updade(TEntity obj)
+        {
+            Db.Entry(obj).State = System.Data.Entity.EntityState.Modified;
+            Db.SaveChanges();
+        }
+    }
+}
---
+++ ProjetoModeloDDD.MVC/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace ProjetoModeloDDD.MVC.AutoMapper
+{
+    public class AutoMapperConfig
+    {
+        public static void REgisterMappings()
+        {
+            Mapper.Initialize(x =>
+            {
+                x.AddProfile<DomainToViewModelMappingProfile>();
+
+                x.AddProfile<ViewModelToDomainMappingProfile>();
+            });
+        }
+    }
+}
